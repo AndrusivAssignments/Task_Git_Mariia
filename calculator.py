0b3305b81cb34c3acdbe54b3c081dda558b29b39
Mariia Andrusiv <mariiaandrusiv@mariias-mbp.speedport.ip>
2024-09-05 20:49:33 +0200

    Add simple CLI calculator app with addition and subtraction functionality

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def calculator(): 
+    expression = input('Calculate: ')
+    to_calculate = expression.replace('', ' ').split()
+    
+    num1 = float(to_calculate[0])
+    num2 = float(to_calculate[-1])
+
+    for symbol in to_calculate: 
+        if symbol == '+': 
+            return num1 + num2 
+        elif symbol == '-':
+            return num1 - num2
+    else:
+        print("Invalid operator. Please use either + or -.")
+        return None
+print(calculator())
